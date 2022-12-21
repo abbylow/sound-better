de3ddc66fe97dc523802279ede4a31eacc7a7209
abbylow <yu950713@gmail.com>
2022-12-21 19:39:12 +0800

    Change meta

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,8 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="SoundBetter" key="title"/>
+        <meta property="og:description" content="Turn your words to be professional" key="description"/>
         <meta
           property="og:image"
           content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ const Home = () => {
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>SoundBetter</title>
       </Head>
       <div className="container">
         <div className="header">
