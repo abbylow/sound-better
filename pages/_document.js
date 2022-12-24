69f28dca6438d0006227ec3e704d2ad6c33c18c4
abbylow <yu950713@gmail.com>
2022-12-25 01:09:12 +0800

    Add more meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -4,13 +4,30 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="SoundBetter" key="title"/>
-        <meta property="og:description" content="Turn your words to be professional" key="description"/>
+        <title>{"SoundBetter - Turn your thoughts to be professional messages"}</title>
+        <link rel="icon" href="/favicon.ico" />
+
+        <meta
+          name="description"
+          content="Struggle with words at work? Let SoundBetter rephrase it professionally for you. Speak up while maintain good impression. "
+          key="desc"
+        />
+
+        <meta property="og:title" content="SoundBetter - Turn your thoughts to be professional messages"/>
+        <meta property="og:description" content="Struggle with words at work? Let SoundBetter rephrase it professionally for you. Speak up while maintain good impression. " />
         <meta
           property="og:image"
           content="https://pub-6064a8cee98d4674ac9f21abc9d5f23e.r2.dev/ogimg.png"
         />
+
         <meta name="twitter:card" content="summary_large_image"></meta>
+        <meta property="twitter:title" content="SoundBetter - Turn your thoughts to be professional messages" />
+        <meta
+          name="twitter:description"
+          content="Struggle with words at work? Let SoundBetter rephrase it professionally for you. Speak up while maintain good impression. "
+        />
+        <meta name="twitter:image" content="https://pub-6064a8cee98d4674ac9f21abc9d5f23e.r2.dev/ogimg.png" />
+
         <script defer data-domain="sound-better-production.up.railway.app" src="/js/script.js"></script>
       </Head>
       <body>
