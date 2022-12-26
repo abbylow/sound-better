18bc13ab0ebb501fca43417cccd868e6c0ecc79d
abbylow <yu950713@gmail.com>
2022-12-26 17:30:06 +0800

    Remove build with buildspace badge

--- pages/index.js
+++ pages/index.js
@@ -81,18 +81,6 @@ const Home = () => {
           </div>
         )}
       </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
-      </div>
     </div>
   );
 };
