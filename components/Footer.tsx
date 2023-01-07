519ce8ffc10ae475687a79869d4e2661b2fe9ca9
abbylow <yu950713@gmail.com>
2023-01-08 02:47:27 +0800

    Add product hunt link and hide irrelevant links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,6 +22,11 @@ const AppFooter: React.FC = () => {
           <Text fw={900} fz="md">© 2022 SoundBetter. All rights reserved.</Text>
 
           <div className={classes.iconWrapper}>
+            <Tooltip label="Feedback here">
+              <a href="https://www.producthunt.com/posts/soundbetter-2?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-soundbetter&#0045;2" target="_blank">
+                <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=374076&theme=light" alt="SoundBetter - A&#0032;tool&#0032;to&#0032;turn&#0032;your&#0032;thoughts&#0032;to&#0032;professional&#0032;messages | Product Hunt" style={{ width: '143px', height: '32px' }} />
+              </a>
+            </Tooltip>
             <Tooltip label="Use in Telegram">
               <ActionIcon size='md' variant='transparent' color="dark" m={4} component="a" href="https://t.me/SoundBetterBot" target="_blank">
                 <IconBrandTelegram size={24} />
@@ -32,16 +37,16 @@ const AppFooter: React.FC = () => {
                 <IconHeart color={'#db2778'} size={24} />
               </ActionIcon>
             </Tooltip>
-            <Tooltip label="Feedback here">
+            {/* <Tooltip label="Feedback here">
               <ActionIcon size='md' variant='transparent' color="dark" m={4} component="a" href="https://twitter.com/abbyotwtofire" target="_blank">
                 <IconBrandTwitter size={24} />
               </ActionIcon>
-            </Tooltip>
-            <Tooltip label="More projects">
-            <ActionIcon size='md' variant='transparent' color="dark" m={4} component="a" href="https://github.com/abbylow" target="_blank">
-              <IconBrandGithub size={24} />
-            </ActionIcon>
-            </Tooltip>
+            </Tooltip> */}
+            {/* <Tooltip label="More projects">
+              <ActionIcon size='md' variant='transparent' color="dark" m={4} component="a" href="https://github.com/abbylow" target="_blank">
+                <IconBrandGithub size={24} />
+              </ActionIcon>
+            </Tooltip> */}
           </div>
         </div>
       </Container>
