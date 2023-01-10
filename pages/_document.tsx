e69d2b69f66dc2a35be1d7a43ddc96e034dd9b67
abbylow <yu950713@gmail.com>
2023-01-10 09:15:52 +0800

    Add buymeacoffee widget

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -37,6 +37,7 @@ export default class _Document extends Document {
             content="Struggle with words at work? Let SoundBetter rephrase it professionally for you. Speak up while maintain good impression. "
           />
           <meta name="twitter:image" content="https://pub-6064a8cee98d4674ac9f21abc9d5f23e.r2.dev/ogimg.png" />
+          <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="abbylow" data-description="Support me on Buy me a coffee!" data-message="" data-color="#5F7FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
         </Head>
         <body>
           <Main />
