2294280ca2b31b42e8c6062a8c1326f783c74937
abbylow <yu950713@gmail.com>
2023-01-20 23:01:20 +0800

    Try replace original input

--- pages/api/slack.ts
+++ pages/api/slack.ts
@@ -5,7 +5,10 @@
 const slackGenerate = async (req, res) => {
   const input = req.body.text?.trim();
   console.log('input ', input)
-  res.status(200).json({text: `I received input ${input}`});
+  res.status(200).json({
+    text: `I received input ${input}`,
+    "replace_original": "true",
+  });
 };
 
 export default slackGenerate;
