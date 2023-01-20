ef06b652a389abb4e11ce44cca33b8099313fbf6
abbylow <yu950713@gmail.com>
2023-01-20 22:18:46 +0800

    Only acknowledge

--- pages/api/slack.ts
+++ pages/api/slack.ts
@@ -1,7 +1,12 @@
+// TODO: Verifying requests from Slack
+// TODO: test what if it is escaping username, channel 
+// TODO: process the input with AI and return 
+// TODO: catch error 
 const slackGenerate = async (req, res) => {
   const input = req.body.text?.trim();
   console.log('input ', input)
-  res.status(200).text(`I received input ${input}`);
+  // res.status(200).json({text: `I received input ${input}`});
+  res.status(200)
 };
 
 export default slackGenerate;
