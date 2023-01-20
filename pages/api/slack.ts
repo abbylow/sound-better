3cbb080c9fdaa01f25f8dacd11e41d5c1492a08b
abbylow <yu950713@gmail.com>
2023-01-20 21:48:39 +0800

    Test if it is ok to return text directly

--- pages/api/slack.ts
+++ pages/api/slack.ts
@@ -1,7 +1,7 @@
 const slackGenerate = async (req, res) => {
   const input = req.body.text?.trim();
   console.log('input ', input)
-  res.status(200).json(`I received input ${input}`);
+  res.status(200).text(`I received input ${input}`);
 };
 
 export default slackGenerate;
