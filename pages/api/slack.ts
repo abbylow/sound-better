49c3c3771fc35adb17bd7bc621372131539b44a3
abbylow <yu950713@gmail.com>
2023-01-20 17:28:30 +0800

    Update log

--- pages/api/slack.ts
+++ pages/api/slack.ts
@@ -1,5 +1,6 @@
 const slackGenerate = async (req, res) => {
-	console.log(`Slack API Input: ${req}`);
+	console.log('req', req)
+  console.log('req.body', req.body)
   res.status(200).json({ output: 'test success' });
 };
 
