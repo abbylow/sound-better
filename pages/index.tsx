5568d12761bcd8490ab8b4fd4215d1fa0f3c3968
abbylow <yu950713@gmail.com>
2023-01-27 17:22:24 +0800

    Swap integration and example position

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,8 @@ const Home: NextPage = () => {
   return (
     <Container size={'lg'} mt={95} p={24} display={'flex'} ta={'center'} sx={{ flexDirection: 'column', alignItems: 'center' }}>
       <Playground />
-      <Examples />
       <Integrations />
+      <Examples />
     </Container>
   );
 };
